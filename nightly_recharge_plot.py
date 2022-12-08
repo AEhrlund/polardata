c9c22f959f2cb5a15934ffeecc88b1a8cbf3e9b1
Alf Ehrlund <alfen99@hotmail.com>
2022-12-08 19:46:36 +0100

    Update

--- nightly_recharge_plot.py
+++ nightly_recharge_plot.py
@@ -1,20 +1,8 @@
 import pandas
+import argparse
 import matplotlib.pyplot as plt
 import lib.database
 import lib.nightly_recharge
-
-data = lib.database.read(lib.nightly_recharge.CSV_FILE, lib.nightly_recharge.get_row_data)
-
-y_axis = []
-dates = []
-value_name = "heart_rate_avg"
-min_value = -1
-max_value = 62
-
-for date_value in data:
-  if date_value[value_name] > min_value and date_value[value_name] < max_value:
-    y_axis.append(date_value[value_name])
-    dates.append(date_value["date"])
 
 
 def plot_data(dates, y_axis):
@@ -28,6 +16,25 @@ def get_moving_average(data, windows_size):
   return ma_data
 
 
-ma_data = get_moving_average(y_axis, 100)
-dates = dates[len(dates) - len(ma_data):]
-plot_data(dates, ma_data)
+if __name__ == '__main__':
+  value_name = "heart_rate_avg"
+  min_value = -1
+  max_value = 65
+
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument("value_name", choices=["hr", "hrv"])
+  args = argparser.parse_args()
+  if args.value_name == "hrv":
+      value_name = "heart_rate_variability_avg"
+
+  y_axis = []
+  dates = []
+  data = lib.database.read(lib.nightly_recharge.CSV_FILE, lib.nightly_recharge.get_row_data)
+  for date_value in data:
+    if date_value[value_name] > min_value and date_value[value_name] < max_value:
+        y_axis.append(date_value[value_name])
+        dates.append(date_value["date"])
+
+  ma_data = get_moving_average(y_axis, 100)
+  dates = dates[len(dates) - len(ma_data):]
+  plot_data(dates, ma_data)
